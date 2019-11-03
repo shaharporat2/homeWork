4daa025cd3d36b58b4c40e6476a6cb805d80462c
shahar <shaharporat2@gmail.com>
2019-11-03 17:46:51 +0200

    first example in lesson

--- 3.11.19/js/functions.js
+++ 3.11.19/js/functions.js
@@ -1,1 +1,20 @@
 /// <reference path="jquery-3.4.1.js" />
+
+
+/*
+
+example from lesson
+
+if we want to display all item in array we use - "of"
+
+if we want to display all props in object we use - "in"
+
+example:
+
+for(const item of array){
+    for(const prop in item){
+        console.log(item[prop]);
+    }
+}
+
+*/
